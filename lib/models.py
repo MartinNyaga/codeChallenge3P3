a2a0ea14b35b9b228cc2292f6007f51a60842e6f
Martin Nyaga <martin.nyaga@student.moringaschool.com>
2023-09-03 12:01:18 +0300

    Added models to db

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,44 @@
 from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String,ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('sqlite:///restaurant_database.db')
 
 Base = declarative_base()
+
+
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
+
+    id = Column(Integer(), primary_key = True)
+    name = Column(String())
+    price = Column(Integer())
+
+    def __repr__(self):
+        return f'Restaurant {self.name}  Price: ${self.price}.00\n'
+    
+
+
+class Customer(Base):
+    __tablename__ = 'customers'
+
+    id = Column(Integer(), primary_key = True)
+    first_name = Column(String())
+    last_name = Column(String())
+
+    def __repr__(self):
+        return f'{self.first_name}, {self.last_name}'
+    
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer(), primary_key=True)
+    restaurant_id = Column(Integer(), ForeignKey('restaurants.id'))
+    description = Column(String())
+    customer_id = Column(Integer(), ForeignKey('customers.id'))
+    star_rating = Column(Integer())
+    
+
+    def __repr__(self):
+        return f'{self.id}, {self.description}, {self.star_rating}'
